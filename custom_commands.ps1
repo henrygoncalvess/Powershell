bbd2cb17878a871fa0a17b8bdbafdc35a37d20cb
henrygoncalvess <octanebt@gmail.com>
2024-10-16 17:45:08 -0300

    melhorando a estrutura do help

--- custom_commands.ps1
+++ custom_commands.ps1
@@ -18,22 +18,62 @@ switch($comando){
     }
 
     "stt"{
-
+        git status
     }
 
     "help" {
-        Write-Host "=== Lista de Comandos Disponíveis: ===" -ForegroundColor Magenta
-        Write-Host "`n1. cc log" -ForegroundColor Cyan -NoNewline
-        Write-Host " - informação dos commits do git" -ForegroundColor Yellow
-        Write-Host "   Parâmetros:"
-        Write-Host "     -o" -ForegroundColor Green -NoNewline
-        Write-Host "   (Opcional)   logs com apenas uma linha.   Padrão: logs com descrição."
-        Write-Host "     [quantidade]" -ForegroundColor Green -NoNewline
-        Write-Host "   (Opcional)   quantidade específica de logs.   Padrão: 5 logs."
+        function colorir {
+            param (
+                [string]$texto,
+                [string]$cor = "White",
+                [string]$nnl
+            )
 
-        Write-Host "`n2. cc stt" -ForegroundColor Cyan -NoNewline
-        Write-Host " - status do workingDirectory e Stage." -ForegroundColor Yellow
+            if ([string]::IsNullOrEmpty($nnl)){
+                Write-Host $texto -ForegroundColor $cor
+            }else{
+                Write-Host $texto -ForegroundColor $cor -NoNewline
+            }
+        }
+
+        colorir "`n`n====== Lista de Comandos Disponíveis ======" Blue
+        colorir "`n   opcional: []     obrigatório: <>" Magenta
+
+
+        
+        # --- --- C O M A N D O --- ---
+        colorir "`n-- cc log" Cyan ' '
+        colorir " [-o] [quantidade]" DarkGray ' '
+        colorir "   informações dos commits do git`n" Yellow
+
+
+        colorir "     [-o]" Green ' '
+        colorir "   logs com apenas uma linha"
+
+        colorir "     [quantidade]" Green ' '
+        colorir "   quantidade específica de logs"
+
+
+        # --- --- C O M A N D O --- ---
+        colorir "`n`n-- cc stt" Cyan ' '
+        colorir " [<opções>]" DarkGray ' '
+        colorir "   Status, WorkingDirectory, Stage, Mudanças`n" Yellow
+
+
+        colorir "     [-rs] <arquivo>" Green ' '
+        colorir "   remove o arquivo do Stage sem alterar o WorkingDirectory"
+
+        colorir "     [-rt] <arquivo>" Green ' '
+        colorir "   restaura WorkingDirectory"
+
+        colorir "     [-d] <arquivo>" Green ' '
+        colorir "   mudanças do Stage pro WorkingDirectory"
+
+        colorir "     [-d] [-s]" Green ' '
+        colorir "   mudanças do último commit pro Stage"
+
+        colorir "`n`n"
     }
 
-    default { Write-Host 'Comando inválido. Digite "cc help" para ver a lista de comandos.' }
+    default { colorir 'Comando inválido. Digite "cc help" para ver a lista de comandos.' }
 }
