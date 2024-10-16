3e77935f208109672a11e9fcb0f271223fd11f03
henrygoncalvess <octanebt@gmail.com>
2024-10-16 12:02:33 -0300

    começando c/ comandos simples

--- custom_commands.ps1
+++ custom_commands.ps1
@@ -1,3 +1,28 @@
-# Set-Alias -Name cc -Value 'C:\Users\#######\Documentos\GitHub\Powershell\custom_commands.ps1'
+$opcoes = @("1 - Histórico de commits", "2 - Listar um diretório", "3 - sair")
+$escolha = $null
 
-Write-Host "Hello World"
+while ($escolha -ne "3") {
+    Write-Host "O que deseja fazer?"
+
+    foreach ($opcao in $opcoes) {
+        Write-Host $opcao
+    }
+
+    $escolha = Read-Host "Escolha uma opção"
+
+    switch ($escolha) {
+        "1" {
+            git log --oneline -6
+            Write-Host
+        }
+
+        "2" {
+            $dir = Read-Host "Digite o caminho do diretório"
+            Write-Host "Listando arquivos em: $dir"
+            Get-ChildItem -Path $dir
+        }
+
+        "3" { "Saindo..." }
+        default { Write-Host "Opção inválida. Tente novamente." }
+    }
+}
