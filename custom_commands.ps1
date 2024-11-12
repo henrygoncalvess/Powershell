d521de791bda5d853ad44c943993705f79ba28bd
henrygoncalvess <octanebt@gmail.com>
2024-11-12 15:58:08 -0300

    modularização funcionando, substituindo arquivos

--- custom_commands.ps1
+++ custom_commands.ps1
@@ -5,52 +5,14 @@ $comandos_globais_path = Join-Path -Path $modulos_path -ChildPath "global_functi
 Import-Module -Name $comandos_git_path
 Import-Module -Name $comandos_globais_path
 
-param (
-    [string]$comando,
-    [switch]$o,
-    [string]$quantidade,
+# comandos globais
+upgrade_posh
+help
 
-    [switch]$rs,
-    [switch]$rt,
-    [switch]$d,
-    [switch]$s
-)
+# comandos git
+git_pull
 
 switch($comando){
-    "upgrade-posh"{
-        $escolha = $null
-
-        while ($escolha -ne "n"){
-            Write-Host 'Deseja atualizar "' -ForegroundColor Cyan -NoNewline
-            Write-Host 'Oh-My-Posh' -ForegroundColor Green -NoNewline
-            Write-Host '"?' -ForegroundColor Cyan -NoNewline
-            Write-Host ' [s/n]: ' -ForegroundColor Yellow -NoNewline
-            $escolha = Read-Host
-
-            if ($escolha -eq "s"){
-                if ($PSVersionTable.PSEdition -eq "Desktop") {
-                    Start-Process "powershell" -ArgumentList "-NoExit"
-                    exit
-
-                } elseif ($env:WT_SESSION) {
-                    wt -w 0 nt -p "PowerShell" ; exit
-                    
-                } elseif ($PSVersionTable.PSEdition -eq "Core") {
-                    Start-Process "pwsh" -ArgumentList "-NoExit"
-                    exit
-
-                }else {
-                    Write-Host = "opção de reinicialização indisponível para este terminal" -ForegroundColor Cyan
-                    Write-Host = "reinicie o terminal para que as mudanças da atualização sejam aplicadas" -ForegroundColor Cyan
-                }
-            }
-        }
-    }
-
-    "p"{
-        git pull origin main
-    }
-
     "log" {
         if ($o -and $quantidade){
             git log --oneline -$quantidade
@@ -109,70 +71,9 @@ switch($comando){
         }
     }
 
-    "help" {
-        function descrever_comando {
-            param (
-                [string]$comando,
-                [string]$funcao,
-                [string]$tabelacao_funcao,
-                [PSCustomObject[]]$parametro_e_descricao = $null
-            )
-
-            # laranja = 255, 133, 3
-            # ansiColor = `e[38;2;${r};${g};${b}m
-            $laranja = "`e[38;2;255;133;3m"
-            $fim = "`e[0m"
-
-            Write-Host "`n`n-- cc " -ForegroundColor Cyan -NoNewline
-            Write-Host "$laranja$comando$fim" -NoNewline
-            if ($null -ne $parametro_e_descricao){
-                Write-Host "`t[<opções>]" -ForegroundColor DarkGray -NoNewline
-            }
-            Write-Host "$tabelacao_funcao$funcao`n" -ForegroundColor Yellow
-
-            foreach ($item in $parametro_e_descricao){
-                Write-Host "     $($item.PSObject.Properties.Name)" -ForegroundColor Green -NoNewline
-                Write-Host "$($item.PSObject.Properties.Value)"
-            }
-        }
-
-        Write-Host "################# Lista de comandos personalizados disponíveis #################" -ForegroundColor Blue
-        Write-Host "`n`t`t`topcional: []     obrigatório: <>" -ForegroundColor Magenta
-
-        descrever_comando "log" "informações dos commits do git" `t @(
-            [PSCustomObject]@{
-                "[-o]" = "`t`t`tlogs com apenas uma linha"
-            },
-            [PSCustomObject]@{
-                "[quantidade]" = "`t`tquantidade específica de logs"
-            }
-        )
-
-        descrever_comando "st" "Status, WorkingDirectory, Stage, Mudanças" `t @(
-            [PSCustomObject]@{
-                "[-rs]" = "`t`t`tremove todos os arquivos do Stage sem alterar o WorkingDirectory"
-            },
-            [PSCustomObject]@{
-                "[-rt]" = "`t`t`tdescarta todas as alterações do WorkingDirectory"
-            },
-            [PSCustomObject]@{
-                "[-d]" = "`t`t`tmostra as mudanças do Stage pro WorkingDirectory"
-            },
-            [PSCustomObject]@{
-                "[-s]" = "`t`t`tmostra as mudanças do último commit pro Stage"
-            }
-        )
-
-        descrever_comando "p" 'executa um pull, trazendo todas as alterações do repositório remoto' `t`t`t`t
-        
-        descrever_comando "upgrade-posh" 'ao iniciar o terminal pergunta se deseja atualizar "Oh-My-Posh"' `t`t
-
-        Write-Host
-    }
-
-    default {
-        Write-Host "`n Comando inválido. Digite" -ForegroundColor Red -NoNewline
-        Write-Host " cc help" -ForegroundColor Green -NoNewline
-        Write-Host " para ver a lista de comandos.`n" -ForegroundColor Red
-    }
+    # default {
+    #     Write-Host "`nComando inválido. Digite" -ForegroundColor Red -NoNewline
+    #     Write-Host " cc help" -ForegroundColor Green -NoNewline
+    #     Write-Host " para ver a lista de comandos.`n" -ForegroundColor Red
+    # }
 }
---
+++ modules/global_functions.psm1
@@ -0,0 +1,78 @@
+function help {
+    function descrever_comando {
+        param (
+            [string]$comando,
+            [string]$funcao,
+            [string]$tabelacao_funcao,
+            [PSCustomObject[]]$parametro_e_descricao = $null
+        )
+
+        # laranja = 255, 133, 3
+        # ansiColor = `e[38;2;${r};${g};${b}m
+        $laranja = "`e[38;2;255;133;3m"
+        $fim = "`e[0m"
+
+        Write-Host "`n`n-- cc " -ForegroundColor Cyan -NoNewline
+        Write-Host "$laranja$comando$fim" -NoNewline
+        if ($null -ne $parametro_e_descricao){
+            Write-Host "`t[<opções>]" -ForegroundColor DarkGray -NoNewline
+        }
+        Write-Host "$tabelacao_funcao$funcao`n" -ForegroundColor Yellow
+
+        foreach ($item in $parametro_e_descricao){
+            Write-Host "     $($item.PSObject.Properties.Name)" -ForegroundColor Green -NoNewline
+            Write-Host "$($item.PSObject.Properties.Value)"
+        }
+    }
+
+    Write-Host "################# Lista de comandos personalizados disponíveis #################" -ForegroundColor Blue
+    Write-Host "`n`t`t`topcional: []     obrigatório: <>" -ForegroundColor Magenta
+
+    descrever_comando "log" "informações dos commits do git" `t @(
+        [PSCustomObject]@{
+            "[-o]" = "`t`t`tlogs com apenas uma linha"
+        },
+        [PSCustomObject]@{
+            "[quantidade]" = "`t`tquantidade específica de logs"
+        }
+    )
+
+    descrever_comando "st" "Status, WorkingDirectory, Stage, Mudanças" `t @(
+        [PSCustomObject]@{
+            "[-rs]" = "`t`t`tremove todos os arquivos do Stage sem alterar o WorkingDirectory"
+        },
+        [PSCustomObject]@{
+            "[-rt]" = "`t`t`tdescarta todas as alterações do WorkingDirectory"
+        },
+        [PSCustomObject]@{
+            "[-d]" = "`t`t`tmostra as mudanças do Stage pro WorkingDirectory"
+        },
+        [PSCustomObject]@{
+            "[-s]" = "`t`t`tmostra as mudanças do último commit pro Stage"
+        }
+    )
+
+    descrever_comando "p" 'executa um pull, trazendo todas as alterações do repositório remoto' `t`t`t`t
+    
+    descrever_comando "upgrade-posh" 'ao iniciar o terminal pergunta se deseja atualizar "Oh-My-Posh"' `t`t
+
+    Write-Host
+}
+
+
+
+function upgrade_posh {
+    $escolha = $null
+
+    while ($escolha -ne "n"){
+        Write-Host 'Deseja atualizar "' -ForegroundColor Cyan -NoNewline
+        Write-Host 'Oh-My-Posh' -ForegroundColor Green -NoNewline
+        Write-Host '"?' -ForegroundColor Cyan -NoNewline
+        Write-Host ' [s/n]: ' -ForegroundColor Yellow -NoNewline
+        $escolha = Read-Host
+
+        if ($escolha -eq "s"){
+            oh-my-posh upgrade --force
+        }
+    }
+}
